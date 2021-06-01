b825523860b3e6796937a984387a37c1cb4395d3
homerjay <homerjay@naver.com>
2021-06-01 10:40:10 +0900

    Home UI prevention, etc...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid19/config/palette.dart';
+import 'package:flutter_covid19/config/style.dart';
+import 'package:flutter_covid19/data/data.dart';
 import 'package:flutter_covid19/widgets/country_dropdown.dart';
 import 'package:flutter_covid19/widgets/custom_appbar.dart';
 
@@ -15,17 +17,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(),
       body: CustomScrollView(
         slivers: [
           _buildheader(screenHeight),
+          _buildPreventionTips(screenHeight),
+          _buildYourOwnTest(screenHeight),
         ],
-      ),);
+      ),
+    );
   }
 
   //appbar랑 이어지는 박스어댑터
@@ -36,15 +38,18 @@ class _HomeScreenState extends State<HomeScreen> {
         decoration: BoxDecoration(
           color: Palette.kPrimaryColor,
           borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),),
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('COVID-19',
+                Text(
+                  'COVID-19',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 25,
@@ -53,13 +58,173 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 //
                 CountryDropdown(
-                  countries: ['CN','FR','IN','IT','UK','USA'],
+                  countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
                   country: _country,
-                  onChanged: (val) => setState(()=> _country=val),
+                  onChanged: (val) => setState(() => _country = val),
                 ),
               ],
             ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Are you feeling sick?',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Text(
+                  'If you feel sic with any COVID-19 symptoms, please call or text us immediately for help',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton.icon(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.call,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Call Now',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    TextButton.icon(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.message_rounded,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Send SMS',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
+  SliverToBoxAdapter _buildPreventionTips(screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Prevention Tips',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: prevention
+                  .map(
+                    (e) => Column(children: [
+                      Image.asset(
+                        //e, keys의 first라면 그대로의 값을 빼온다
+                        e.keys.first,
+                        height: screenHeight * 0.12,
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.015,
+                      ),
+                      //e.values.first의 값의 그대로를 빼온다.
+                      Text(
+                        e.values.first,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ]),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildYourOwnTest(screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20,
+        ),
+        padding: EdgeInsets.all(10),
+        height: screenHeight * 0.15,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFAD9FE4), Palette.kPrimaryColor],
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset('assets/images/own_test.png'),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Do your own Test!',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: screenHeight * 0.01,),
+                Text('Follow the instructions\nto do your own test.',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                  ),
+                  maxLines: 2,
+                ),
+              ],
+            ),
           ],
         ),
       ),
